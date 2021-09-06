92094142ccbb0c0ff8ba004c14b5d2ab00391543
sanketwakhare <sanketwakhare@gmail.com>
2021-09-06 11:19:13 +0530

    2 files modified

--- src/components/news/newscontainer/newsitem/NewsItem.js
+++ src/components/news/newscontainer/newsitem/NewsItem.js
@@ -1,13 +1,14 @@
-import React from "react";
-import "./newsitem.css";
+import React from 'react';
+import './newsitem.css';
 
 export default function NewsItem(props) {
   const { singleNewsItem } = props;
   return (
     <>
       <div className="newsItem">
-        <h1>{singleNewsItem ? singleNewsItem.title : "No news Item"}</h1>
-        <p>{singleNewsItem ? singleNewsItem.contents : "No content"}</p>
+        <h1>{singleNewsItem ? singleNewsItem.title : 'No news Item'}</h1>
+        <hr className="seperator" />
+        <p>{singleNewsItem ? singleNewsItem.contents : 'No content'}</p>
       </div>
     </>
   );
